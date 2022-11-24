e4848d556fb94cdbfab1936055cc492b185d29fa
Matheus <matheus-rodrigues37@live.com>
2022-11-24 16:22:56 -0300

    up

--- pages/search.tsx
+++ pages/search.tsx
@@ -66,15 +66,28 @@ const Search = () => {
 
   return (
     <div className="bg-slate-100 dark:bg-slate-800 dark:text-slate-50 px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-      <Grid>
-        {
-          actualFilterPokemon?.map((pokemon: PokemonDetails) => (
-            <Link href={pokemon.name} key={pokemon.id}>
+      {
+        (actualFilterPokemon.length) ? actualFilterPokemon?.map((pokemon: PokemonDetails) => (
+          <Grid key={pokemon.id}>
+            <Link href={pokemon.name} >
               <Pokemon image={pokemon.sprites.front_default} text={pokemon.name} types={pokemon.types} />
             </Link>
-          ))
-        }
-      </Grid>
+          </Grid>
+        )) : (
+          <section className="flex flex-col h-[80vh] justify-center items-center  bg-slate-100 dark:bg-slate-800 dark:text-gray-100">
+            <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+              <div className="max-w-md text-center">
+                <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+                  <span className="sr-only">Error</span>404
+                </h2>
+                <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn{"'"}t find this page.</p>
+                <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
+                <a rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded bg-slate-300 dark:bg-slate-400 dark:text-gray-900">Back to homepage</a>
+              </div>
+            </div>
+          </section>
+        )
+      }
       <div className="w-100 mx-auto">
         <Pagination
           current={currentPage}
