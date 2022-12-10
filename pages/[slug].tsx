81ac4bd6ab610f1dc7e39bd21eee81578e75446e
Matheus <matheus-rodrigues37@live.com>
2022-12-10 16:16:03 -0500

    up

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from "next";
-import Link from "next/link";
+import Pokemon from "../components/Pokemon";
 import { PokemonDetails } from "../types/pokemonDetails";
 
 interface DetailsProps {
@@ -8,18 +8,9 @@ interface DetailsProps {
 
 function Details({ data }: DetailsProps) {
   return (
-    <div>
-      <section className="flex py-44 justify-center items-center bg-slate-100 dark:bg-slate-800 dark:text-gray-100">
-        <div className="container flex flex-col items-center justify-center px-5 mx-auto ">
-          <div className="max-w-md text-center">
-            <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
-              <span className="sr-only">Error</span>404
-            </h2>
-            <p className="text-2xl font-semibold md:text-3xl">Desculpe, não foi encontrado nenhum resultado.</p>
-            <p className="mt-4 mb-8 dark:text-gray-400">Mas não se preocupe, você pode procurar por vários pokemons e saber suas características :).</p>
-            <Link href="/" className="px-8 py-3 font-semibold rounded bg-slate-300 dark:bg-slate-400 dark:text-gray-900">De volta para o Início</Link>
-          </div>
-        </div>
+    <div className="flex justify-center items-center h-[calc(100vh-200px)] w-screen">
+      <section className="flex justify-center items-center bg-slate-100 dark:bg-slate-800 dark:text-gray-100">
+        <Pokemon image={data.sprites.front_default} text={data.name} types={data.types} />
       </section>
     </div>
   );
