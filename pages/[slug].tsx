29e720f8f6d435afa52e15a118eb784ac86cc6c9
Matheus <matheus-rodrigues37@live.com>
2022-12-11 12:25:37 -0300

    up

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -8,63 +8,51 @@ interface DetailsProps {
 
 function Details({ data }: DetailsProps) {
   return (
-    <div className="flex flex-col md:flex-row h-full md:h-[calc(100vh-73px)]">
-      <div className="bg-slate-100 dark:bg-slate-800 dark:text-gray-100 w-full md:w-1/2 px-12 flex items-center justify-center flex-col">
-        <div className="carousel w-full bg-slate-100 dark:bg-slate-800 py-10">
-          <div id="item1" className="carousel-item w-full">
-            <img src={data.sprites.front_default} className="w-full" alt="pokemon" />
-          </div>
-          <div id="item2" className="carousel-item w-full">
-            <img src={data.sprites.back_default} className="w-full" alt="pokemon" />
-          </div>
-          <div id="item3" className="carousel-item w-full">
-            <img src={data.sprites.front_shiny} className="w-full" alt="pokemon" />
-          </div>
-          <div id="item4" className="carousel-item w-full">
-            <img src={data.sprites.back_shiny} className="w-full" alt="pokemon" />
-          </div>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 w-full py-2 gap-2">
-          <a href="#item1" className="btn btn-xs h-full border-none bg-slate-300 dark:bg-slate-500 dark:hover:bg-slate-300 hover:bg-slate-500" title="pokemon"><img src={data.sprites.front_default} alt="front" /></a>
-          <a href="#item2" className="btn btn-xs h-full border-none bg-slate-300 dark:bg-slate-500 dark:hover:bg-slate-300 hover:bg-slate-500" title="pokemon"><img src={data.sprites.back_default} alt="back" /></a>
-          <a href="#item3" className="btn btn-xs h-full border-none bg-slate-300 dark:bg-slate-500 dark:hover:bg-slate-300 hover:bg-slate-500" title="pokemon"><img src={data.sprites.front_shiny} alt="front_shiny" /></a>
-          <a href="#item4" className="btn btn-xs h-full border-none bg-slate-300 dark:bg-slate-500 dark:hover:bg-slate-300 hover:bg-slate-500" title="pokemon"><img src={data.sprites.back_shiny} alt="back_shiny" /></a>
-        </div>
-      </div>
-      <div className="flex flex-col justify-center items-center w-full md:w-1/2 px-12">
-        <h1 className="text-4xl text-slate-800 mb-10 dark:text-white">{data.name}</h1>
-        <h2 className="text-slate-800 text-2xl mb-5 dark:text-white">Habilidades</h2>
-        {data.abilities.map((ability, i) => <p key={i} className="flex gap-6 text-slate-800 dark:text-white">{ability.ability.name}</p>)}
-        <div className="flex flex-col">
-          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="overflow-hidden">
-                <table className="min-w-full">
-                  <tbody>
-                    <tr className="bg-slate-100 dark:bg-slate-800 dark:border-slate-100 border-b border-slate-800">
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-slate-800 mb-10 dark:text-white">1</td>
-                      <td className="text-sm text-slate-800 mb-10 dark:text-white font-light px-6 py-4 whitespace-nowrap">
-                        Mark
-                      </td>
-                    </tr>
-                    <tr className="bg-slate-100 dark:bg-slate-800">
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-slate-800 mb-10 dark:text-white">2</td>
-                      <td className="text-sm text-slate-800 mb-10 dark:text-white font-light px-6 py-4 whitespace-nowrap">
-                        Jacob
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+    <>
+      <h1 className="text-4xl text-slate-800 mb-10 dark:text-white">{data.name.toUpperCase()}</h1>
+      <div className="flex flex-col md:flex-row h-full md:h-[calc(100vh-152px)]">
+        <div className="bg-slate-100 dark:bg-slate-800 dark:text-gray-100 w-full md:w-1/2 px-12 flex items-center justify-center flex-col">
+          <div className="carousel w-full bg-slate-100 dark:bg-slate-800 py-10">
+            <div id="item1" className="carousel-item w-full">
+              <img src={data.sprites.front_default} className="w-full" alt="pokemon" />
+            </div>
+            <div id="item2" className="carousel-item w-full">
+              <img src={data.sprites.back_default} className="w-full" alt="pokemon" />
+            </div>
+            <div id="item3" className="carousel-item w-full">
+              <img src={data.sprites.front_shiny} className="w-full" alt="pokemon" />
+            </div>
+            <div id="item4" className="carousel-item w-full">
+              <img src={data.sprites.back_shiny} className="w-full" alt="pokemon" />
             </div>
           </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 w-full py-2 gap-2">
+            <a href="#item1" className="btn btn-xs h-full border-none bg-slate-300 dark:bg-slate-500 dark:hover:bg-slate-300 hover:bg-slate-500" title="pokemon"><img src={data.sprites.front_default} alt="front" /></a>
+            <a href="#item2" className="btn btn-xs h-full border-none bg-slate-300 dark:bg-slate-500 dark:hover:bg-slate-300 hover:bg-slate-500" title="pokemon"><img src={data.sprites.back_default} alt="back" /></a>
+            <a href="#item3" className="btn btn-xs h-full border-none bg-slate-300 dark:bg-slate-500 dark:hover:bg-slate-300 hover:bg-slate-500" title="pokemon"><img src={data.sprites.front_shiny} alt="front_shiny" /></a>
+            <a href="#item4" className="btn btn-xs h-full border-none bg-slate-300 dark:bg-slate-500 dark:hover:bg-slate-300 hover:bg-slate-500" title="pokemon"><img src={data.sprites.back_shiny} alt="back_shiny" /></a>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center w-full md:w-1/2 px-12 gap-6">
+          <h2 className="text-slate-800 text-2xl mb-5 dark:text-white">Habilidades</h2>
+          {data.abilities.map((ability, i) => (<div key={i} className="flex gap-6"><span className="text-slate-800 dark:text-white">{ability.ability.name}</span></div>))}
+          <table className="flex flex-col w-fit justify-around">
+            <tr className="bg-slate-100 dark:bg-slate-800 dark:border-slate-100 border-slate-800 flex flex-col justify-evenly ">
+              {data.stats.map((stat, i) => (
+                <div key={i}>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-slate-800 mb-10 dark:text-white w-48 border-b border-slate-600">{stat.stat.name}</td>
+                  <td className="text-sm text-slate-800 mb-10 dark:text-white font-light px-6 py-4 whitespace-nowrap w-48 border-b text-center border-slate-600">{stat.base_stat}</td>
+                </div>
+              ))}
+            </tr>
+          </table>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export default Details;
+export default Details
 
 export const getStaticProps: GetStaticProps = async context => {
   const slug = context.params?.slug
