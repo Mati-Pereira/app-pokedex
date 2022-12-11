6e6556177efd0f3f8c37131693b214d7a78aae5f
Matheus <matheus-rodrigues37@live.com>
2022-12-11 11:11:28 -0300

    up

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
 import { GetStaticPaths, GetStaticProps } from "next";
-import Pokemon from "../components/Pokemon";
 import { PokemonDetails } from "../types/pokemonDetails";
 
 interface DetailsProps {
@@ -8,10 +8,32 @@ interface DetailsProps {
 
 function Details({ data }: DetailsProps) {
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-200px)] w-screen">
-      <section className="flex justify-center items-center bg-slate-100 dark:bg-slate-800 dark:text-gray-100">
-        <Pokemon image={data.sprites.front_default} text={data.name} types={data.types} />
-      </section>
+    <div className="flex flex-col md:flex-row h-full md:h-[calc(100vh-73px)]">
+      <div className="bg-slate-100 dark:bg-slate-800 dark:text-gray-100 w-full md:w-1/2 px-12 flex items-center justify-center flex-col">
+        <div className="carousel w-full bg-slate-100 dark:bg-slate-800 py-10">
+          <div id="item1" className="carousel-item w-full">
+            <img src={data.sprites.front_default} className="w-full" alt="pokemon" />
+          </div>
+          <div id="item2" className="carousel-item w-full">
+            <img src={data.sprites.back_default} className="w-full" alt="pokemon" />
+          </div>
+          <div id="item3" className="carousel-item w-full">
+            <img src={data.sprites.front_shiny} className="w-full" alt="pokemon" />
+          </div>
+          <div id="item4" className="carousel-item w-full">
+            <img src={data.sprites.back_shiny} className="w-full" alt="pokemon" />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 w-full py-2 gap-2">
+          <a href="#item1" className="btn btn-xs h-full" title="pokemon"><img src={data.sprites.front_default} alt="front" /></a>
+          <a href="#item2" className="btn btn-xs h-full" title="pokemon"><img src={data.sprites.back_default} alt="back" /></a>
+          <a href="#item3" className="btn btn-xs h-full" title="pokemon"><img src={data.sprites.front_shiny} alt="front_shiny" /></a>
+          <a href="#item4" className="btn btn-xs h-full" title="pokemon"><img src={data.sprites.back_shiny} alt="back_shiny" /></a>
+        </div>
+      </div>
+      <div className="flex justify-center items-center w-full md:w-1/2 px-12">
+        dsfgadsfgasd
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/react-tailwindcss-select/dist/index.esm.js",
   ],
   darkMode: "class",
   theme: {
@@ -12,5 +11,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
